ecbd22816418f642b91e3c25e3ca56c0ffedb6f1
Florin M <florinmaracine@yahoo.com>
2022-11-08 21:57:17 +0000

    Moved JS files to assets/scripts/ folder

---
+++ assets/scripts/gmaps-earth.js
@@ -0,0 +1,40 @@
+// SOURCE: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+function gMapsMap() {
+    var locations = [
+        ["Tomis", 44.171006, 28.661011, 1],
+        ["Ancient Histria Citadel", 44.546676, 28.773069, 2],
+        ["Calatis", 43.814692, 28.586530, 3],
+    ];
+
+    var map = new google.maps.Map(document.getElementById("map_1"), {
+        zoom: 8,
+        center: new google.maps.LatLng(44.19, 28.56),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+   
+    var citadelIcon = "../assets/images/temple_ruins.png";
+
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            icon: citadelIcon,
+        });
+
+        google.maps.event.addListener(
+            marker,
+            "click",
+            (function (marker, i) {
+                return function () {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                };
+            })(marker, i)
+        );
+    }
+}
---
+++ assets/scripts/wiki-fetch.js
@@ -0,0 +1,93 @@
+// Base code SOURCE:  https://femkreations.com/how-to-build-a-wikipedia-search-app-in-9-steps/
+
+let fetchedInfoResponse = "";
+let searchedItem = "";
+let searchingItem = "";
+
+// this function is called when the user clicks the selected city Wikepedia tab/button:
+function fetchWiki(id) {
+    // Getting the id of the city-button clicked:
+    searchedItem = document.getElementById(id);
+    // Split the id to the city name only:
+    searchingItem = searchedItem.id.split('_')[0];
+    // Getting the coresponding  city div to print the results on:
+    fetchedInfoResponse = document.getElementById(searchingItem + '-wiki-fetching');
+    // Calling the fetchWikiResults function, passing the city name as a parameter, to fetch the results:
+    fetchWikiResults(searchingItem);
+}
+
+// This function fetches the results from the Wikipedia API:
+function fetchWikiResults(x) {
+    // let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${x}`;
+    let url = "https://en.wikipedia.org/w/api.php?" +
+        new URLSearchParams({
+            "action": "query",
+            "list": "search",
+            "prop": "info",
+            "inprop": "url",
+            "utf8": "",
+            "format": "json",
+            "origin": "*",
+            "srlimit": "7",
+            "srsearch": `${searchingItem}`
+        });
+    // Promise fetching and processing the response from the API:
+    fetch(url)
+        .then(function(response) {
+            // getting the response data in JSON format
+            return (response.json());
+        })
+        .then(function(data) {
+            // saving the query objects in an array:
+            let resultsArray = data.query.search;
+            // calling the printResultsOnPage function, passing the array as a parameter:
+            printResultsOnPage(resultsArray);
+        })
+        .catch(function() {
+            console.log('An error occured');
+            printErrorOnPage();
+        });
+}
+
+// function to print the results on the wikipedia tab:
+function printResultsOnPage(myArray) {
+    fetchedInfoResponse.innerHTML = " ";
+    fetchedInfoResponse.insertAdjacentHTML('beforeend', `<h2>Results for \"${searchingItem}\" </h2>`);
+    myArray.forEach(function(item) {
+        let itemTitle = item.title;
+        let itemSnippet = item.snippet;
+        let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
+
+        fetchedInfoResponse.insertAdjacentHTML('beforeend',
+            `<div class="resultItem">
+         <h3 class="resultTitle">
+          <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
+         </h3>
+         <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
+         ${itemSnippet}</a></p>
+        </div>`
+        );
+    })
+}
+
+// function to print the results on the wikipedia tab:
+function printErrorOnPage() {
+    fetchedInfoResponse.innerHTML = " ";
+    fetchedInfoResponse.insertAdjacentHTML('beforeend', `<h2>Could not retrieve the results for \"${searchingItem}\" </h2>`);
+    // myArray.forEach(function(item) {
+    //     let itemTitle = item.title;
+    //     let itemSnippet = item.snippet;
+    //     let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
+
+    //     fetchedInfoResponse.insertAdjacentHTML('beforeend',
+    //         `<div class="resultItem">
+    //      <h3 class="resultTitle">
+    //       <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
+    //      </h3>
+    //      <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
+    //      ${itemSnippet}</a></p>
+    //     </div>`
+    //     );
+    // })
+
+}
