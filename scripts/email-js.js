2a32ecbd177e4829ee146d3f089b25559b0cadec
phloreenm <florinmaracine@yahoo.com>
2022-11-23 13:21:13 +0000

    Implemented sending email from Contact page using EmailJS API

--- scripts/email-js.js
+++ scripts/email-js.js
@@ -1,13 +1,30 @@
-// const PUBLIC_KEY = '8XKDNSzlw_svug1Gk';
-    (function () {
-      emailjs.init("8XKDNSzlw_svug1Gk");
-      emailjs.send("phlopping_gmail_service", "gc-site", {
-        name: "Ioana",
-        notes: "Check this out!"
-      })
-      .then(function(response) {
-        console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-      }, function(err) {
-        console.log("FAILED. error=", err);
-      });
-    })();
+function sendMail(contactForm) {
+  emailjs.send("phlopping_gmail_service", "gc-site", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "message": contactForm.message.value,
+  })
+  .then(
+      function(response) {
+          console.log("SUCCESS", response);
+      },
+      function(error) {
+          console.log("FAILED", error);
+      }
+  );
+  return false;  // To block from loading a new page
+}
+// console.log("email.js loaded");
+// console.log("before sendMail() call");
+// function sendMail(contactForm) {
+//   emailjs.send("phlopping_gmail_service", "gc-site", {
+//       from_name: contactForm.name.value,
+//       from_email: contactForm.emailaddress.value,
+//       message: contactForm.message.value,
+//     })
+//     .then(function (response) {
+//       console.log("SUCCESS. Mail sent succesfully! status=%d, text=%s", response.status, response.text);
+//     }, function (err) {
+//       console.log("FAILED. error=", err);
+//     });
+// };
