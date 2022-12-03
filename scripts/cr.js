d7f306471b8939f1d2e39419e8a899e9bf00e92d
Florin M <florinmaracine@yahoo.com>
2022-12-03 11:23:47 +0000

    Added margin-top to Copyright content

--- scripts/cr.js
+++ scripts/cr.js
@@ -1,3 +1,3 @@
 /*jshint esversion: 6 */
-const cr = `<p>Copyright <i class="fa-regular fa-copyright"></i> ${new Date().getFullYear()} <a href="https://github.com/phloreenm/MP2-JS--CI-CoBC-FM" target="_blank">phloreenm</a></p>`;
-    document.getElementById('copyright').innerHTML = cr;
+const cr = `<p style="margin-top: 1rem; margin-bottom: 1rem;">Copyright <i class="fa-regular fa-copyright"></i> ${new Date().getFullYear()} <a href="https://github.com/phloreenm/MP2-JS--CI-CoBC-FM" target="_blank">phloreenm</a></p>`;
+document.getElementById('copyright').innerHTML = cr;
