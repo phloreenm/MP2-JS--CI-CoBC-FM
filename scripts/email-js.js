3e064902dbd34e972afc428923a1de1ace0ae71e
Florin M <florinmaracine@yahoo.com>
2022-12-03 13:08:21 +0000

    Contact page-> when success display success page; when not, reload page and show alert

--- scripts/email-js.js
+++ scripts/email-js.js
@@ -1,27 +1,31 @@
 /*jshint esversion: 6 */
 // Source code: https://github.com/Code-Institute-Solutions/InteractiveFrontendDevelopment-Resume/blob/master/03-SendingEmailsUsingEmailJS/06-sending_emails/assets/js/sendEmail.js
 function sendMail(contactForm) {
+
     emailjs.send("phlopping_gmail_service", "gc-site", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "message": contactForm.message.value,
         })
         .then(
-            function (response) {
+            function(response) {
                 console.log("SUCCESS", response);
                 console.log('Email sent successfully! response.status: ', response.status);
-                displayModalMailStatus();
+                redirectPage();
             },
-            function (error) {
+            function(error) {
                 console.log("Email failed to send");
                 console.log("FAILED", error);
+                window.location.href = "contact.html";
+                alert("Email couldn't be sent. Please check your internet connection and try again!");
             }
         );
     // To block from loading a new page
     return false;
 }
 
-function displayModalMailStatus() {
-    console.log("displayModalMailStatus function called");
-    alert('Email sent Successfully');
+function redirectPage() {
+    console.log("redirectPage function called");
+    // alert('Email sent Successfully');
+    window.location.href = "success.html";
 }
