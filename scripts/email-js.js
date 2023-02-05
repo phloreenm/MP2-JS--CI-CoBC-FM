ff4ea7f72d763b631962a09e0ed1fc103fb40915
Florin M <40276006+phloreenm@users.noreply.github.com>
2023-02-05 23:20:06 +0000

    Removed unused comments

--- scripts/email-js.js
+++ scripts/email-js.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 // Source code: https://github.com/Code-Institute-Solutions/InteractiveFrontendDevelopment-Resume/blob/master/03-SendingEmailsUsingEmailJS/06-sending_emails/assets/js/sendEmail.js
 function sendMail(contactForm) {
-
     emailjs.send("phlopping_gmail_service", "gc-site", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
@@ -9,14 +8,9 @@ function sendMail(contactForm) {
         })
         .then(
             function(response) {
-                // console.log("SUCCESS", response);
-                // console.log('Email sent successfully! response.status: ', response.status);
                 redirectPage();
             },
             function(error) {
-                // console.log("Email failed to send");
-                // console.log("FAILED", error);
-                // window.location.href = "contact.html";
                 alert("Email couldn't be sent. Please check your internet connection and try again!");
             }
         );
@@ -26,6 +20,5 @@ function sendMail(contactForm) {
 
 function redirectPage() {
     console.log("redirectPage function called");
-    // alert('Email sent Successfully');
     window.location.href = "success.html";
 }
